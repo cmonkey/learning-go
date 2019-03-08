eb6e638fe74e7aa0cea632da1f2742d16033426e
CMonkey <42.codemonkey@gmail.com>
2019-03-08 10:19:55 +0800

    add shorter func

--- src/learning-go/main.go
+++ src/learning-go/main.go
@@ -10,4 +10,22 @@ func main(){
 	fmt.Println("hello World!") // write to stdout
 	println("Hello World!") // write to stderr
 
+	const a bool = true
+	const b int = 42
+	const c float32 = 3.1415
+	const d string = "cmonkey"
+	const e complex64 = 12 + 5i
+	fmt.Println(a, b, c, d, e)
+
+	shorter()
+
+}
+
+func shorter(){
+	a := true
+	b := 42
+	c := 3.1415
+	d := "cmonkey"
+	e := 12 + 5i
+	fmt.Println(a, b, c , d, e)
 }
