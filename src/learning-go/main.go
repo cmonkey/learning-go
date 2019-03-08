75732047a03fa074ddb8b712bc6da15acdd279b0
CMonkey <42.codemonkey@gmail.com>
2019-03-08 10:16:19 +0800

    init learning-go/main.go

---
+++ src/learning-go/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main(){
+	log.Println("Hello World!") // write to stdlog
+	fmt.Println("hello World!") // write to stdout
+	println("Hello World!") // write to stderr
+
+}
